f70cd8709ee53099e2573690a98be36dfeaf3156
Anne Rehill <arehill@umich.edu>
2021-02-15 20:49:53 -0500

    Annie's file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Annie from './member_pages/annie';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Annie/>
     </div>
   );
 }
